20bfe6577cb8d1bff1013c11588da20c65b230e7
taylordeckard <taylor999@gmail.com>
2021-12-08 23:23:36 -0500

    Add horizontal collision detection

--- src/hooks/useGravity.tsx
+++ src/hooks/useGravity.tsx
@@ -1,13 +1,13 @@
 import { MutableRefObject, useContext, useEffect, useState } from 'react';
 import { StateContext } from '../StateProvider';
 import { useFrame } from '@react-three/fiber';
-import { Box3, Mesh } from 'three';
+import { Box3, Object3D } from 'three';
 import {
   BOUNDARY_MIN_Y,
   TMINO_STARTING_Y_MAP,
 } from '../constants';
 
-export function useGravity (activePiece: MutableRefObject<Mesh>) {
+export function useGravity (activePiece: MutableRefObject<Object3D>) {
   const { state } = useContext(StateContext);
   const [y, setY] = useState(16.5);
   const [reset, setReset] = useState(0);
@@ -53,7 +53,7 @@ export function useGravity (activePiece: MutableRefObject<Mesh>) {
   }
 
   function intersectsLocked () {
-    return [...activePiece.current.children].some(mesh => {
+    return [...(activePiece.current?.children ?? [])].some(mesh => {
       const mBox = new Box3();
       mBox.setFromObject(mesh);
       return state.lockedObjects.some(obj => {
@@ -61,8 +61,8 @@ export function useGravity (activePiece: MutableRefObject<Mesh>) {
         box.setFromObject(obj);
         const boxX = Math.round((Math.round(box.min.x) + Math.round(box.max.x)) / 2);
         const meshX = Math.round((Math.round(mBox.min.x) + Math.round(mBox.max.x)) / 2);
-        const roundedBoxMinY = Math.round(box.min.y * 10) / 10;
-        const roundedMeshMinY = Math.round(mBox.min.y * 10) / 10;
+        const roundedBoxMinY = (Math.round(box.min.y * 10) / 10);
+        const roundedMeshMinY = (Math.round(mBox.min.y * 10) / 10);
         return meshX === boxX && roundedBoxMinY === roundedMeshMinY;
       });
     })
@@ -75,7 +75,6 @@ export function useGravity (activePiece: MutableRefObject<Mesh>) {
         activePiece.current.position.y = y + 1;
         setReset(reset + 1);
         setY(TMINO_STARTING_Y_MAP[state.nextTetromino]);
-        console.log(state.nextTetromino);
       }
     }
   });
--- src/hooks/usePositionControls.tsx
+++ src/hooks/usePositionControls.tsx
@@ -1,4 +1,5 @@
-import { MutableRefObject, useEffect, useState } from 'react';
+import { MutableRefObject, useContext, useEffect, useRef, useState } from 'react';
+import { StateContext } from '../StateProvider';
 import { useFrame } from '@react-three/fiber';
 import { Box3, Group } from 'three';
 import {
@@ -12,6 +13,9 @@ export function usePositionControls (
   pieceLetter: string,
   reset: number,
 ) {
+  const { state } = useContext(StateContext);
+  const stateRef = useRef(state);
+  useEffect(() => { stateRef.current = state; }, [state]);
   const [xOffset, setXOffset] = useState(0);
   useEffect(() => {
     setXOffset(TMINO_STARTING_X_MAP[pieceLetter]);
@@ -20,10 +24,18 @@ export function usePositionControls (
     const arrowRightLeftHandler = (event: KeyboardEvent) => {
       const box = new Box3();
       box.setFromObject(ref.current);
-      if (event.key === 'ArrowLeft' && isInsideBoundary(box, -1)) {
+      if (
+        event.key === 'ArrowLeft'
+        && isInsideBoundary(box, -1)
+        && !intersectsLocked(-1)
+      ) {
         setXOffset(x => x - 1);
       }
-      if (event.key === 'ArrowRight' && isInsideBoundary(box, 1)) {
+      if (
+        event.key === 'ArrowRight'
+        && isInsideBoundary(box, 1)
+        && !intersectsLocked(1)
+      ) {
         setXOffset(x => x + 1);
       }
     };
@@ -34,6 +46,21 @@ export function usePositionControls (
   function isInsideBoundary (box: Box3, offset: number) {
     return ((box.min.x + offset) >= BOUNDARY_MIN_X)
       && ((box.max.x + offset) <= BOUNDARY_MAX_X);
+  }
+  function intersectsLocked (offset: number) {
+    return [...(ref.current?.children ?? [])].some(mesh => {
+      const mBox = new Box3();
+      mBox.setFromObject(mesh);
+      return stateRef.current.lockedObjects.some(obj => {
+        const box = new Box3();
+        box.setFromObject(obj);
+          const boxY = Math.round((Math.round(box.min.y) + Math.round(box.max.y)) / 2);
+          const meshY = Math.round((Math.round(mBox.min.y) + Math.round(mBox.max.y)) / 2);
+          const roundedBoxMinX = (Math.round(box.min.x * 10) / 10);
+          const roundedMeshMinX = (Math.round(mBox.min.x * 10) / 10) + offset;
+          return meshY === boxY && roundedBoxMinX === roundedMeshMinX;
+      });
+    })
   }
   useFrame(() => {
     if (ref.current) {
